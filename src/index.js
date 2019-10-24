9008437806e259db04f78b08a5c8002fcc7d1999
Dennis Padiernos <depadiernos@gmail.com>
2019-10-23 21:22:24 -0500

    implement redux store and import reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+import { reducer as additionalPrice } from './reducers/additionalPriceReducer'
+import { reducer as car } from './reducers/carReducer'
+import { reducer as additionalFeatures } from './reducers/additionalFeaturesReducer'
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const rootReducer = combineReducers({
+    additionalPrice,
+    car,
+    additionalFeatures
+});
+
+const store = createStore(rootReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement);
